a96df0e993dc423a79eef2c38e14de8301aa65e6
rushi-00 <131697982+rushi-00@users.noreply.github.com>
2023-09-11 19:19:53 +0530

    Add files via upload
    move all the zeros at the end of array.

---
+++ assignment2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{               
+    int n;
+    cout<<"Enter thr size of array :";
+    cin>>n;
+    int A[n];
+    cout <<"Enter the element :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>A[i];
+    }
+
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (A[i] != 0) {
+			swap(A[j], A[i]); // Partitioning the array
+			j++;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout <<"New Array"<< A[i] << " "; // Print the array
+	}
+
+	return 0;
+}
